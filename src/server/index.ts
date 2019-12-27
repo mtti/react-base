b0b2f7ddb08c31fb078f8f212c74c64254b12f48
mtti <mtti@users.noreply.github.com>
2019-12-27 09:18:35 +0200

    Add backend linting and tests

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,6 +6,6 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'))
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 app.listen(port);
